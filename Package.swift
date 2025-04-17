3c927d4697f539aebd19ef2f80ec01b3b075917c
Jared Davidson (Archetapp) <archetapp@gmail.com>
2025-04-17 17:39:19 -0600

    update

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "RenderLock",
     platforms: [
         .iOS(.v14),
-        .visionOS(.v1)
+        .visionOS(.v1),
+        .macOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
